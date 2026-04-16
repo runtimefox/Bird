69c4121e7f6feca7710be1d5f425fd5544d393d8
ComradeUa <az63.zov@gmail.com>
2026-04-16 13:10:30 +0300

    fix:update url in service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,7 +12,7 @@ class UserService {
     return response;
   }
   async updateProfile(data: FormData) {
-    const response = await axiosAuth.patch<TypeUserResponse>(`${this.URL}/profile`, data, {
+    const response = await axiosAuth.patch<TypeUserResponse>(`${this.URL}/update`, data, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
