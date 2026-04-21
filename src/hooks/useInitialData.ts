b6da576101f5de5e1cd2fe5829a6be3b37ab72cc
ComradeUa <az63.zov@gmail.com>
2026-04-21 13:08:13 +0300

    refactor: remove initialized ref and guard in useInitialData

--- src/hooks/useInitialData.ts
+++ src/hooks/useInitialData.ts
@@ -1,16 +1,13 @@
 import type { UseFormReset } from 'react-hook-form';
 import { useGetProfile } from './useGetProfile';
 import type { TypeUserForm } from '@/types/auth.type';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 export const useInitialData = (reset: UseFormReset<TypeUserForm>) => {
   const { data, isSuccess } = useGetProfile();
-  const initialized = useRef(false);
 
   useEffect(() => {
-    console.log('isSuccess:', isSuccess, 'data:', data, 'initialized:', initialized.current);
-    if (isSuccess && data && !initialized.current) {
-      initialized.current = true;
+    if (isSuccess && data) {
       reset({
         email: data.data.email,
         name: data.data.name,
