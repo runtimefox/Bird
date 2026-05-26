1e6397133a7dc651729ff077ef39ef3a1af3e5b8
ComradeUa <az63.zov@gmail.com>
2026-05-26 11:00:00 +0300

    refactor: improve proxy function to handle root page redirection

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,8 +5,12 @@ import { DASHBOARD } from './config/menu.config';
 export async function proxy(request: NextRequest) {
   const { url, cookies } = request;
   const refreshToken = cookies.get(ENUM_AUTH_TOKEN_TYPE.REFRESH_TOKEN)?.value;
-
+  const isRootPage = new URL(url).pathname === '/';
   const isAuthPage = url.includes('/auth');
+
+  if (isRootPage) {
+    return NextResponse.redirect(new URL(refreshToken ? DASHBOARD.HOME : '/auth/', url));
+  }
 
   if (isAuthPage && refreshToken) {
     return NextResponse.redirect(new URL(DASHBOARD.HOME, url));
